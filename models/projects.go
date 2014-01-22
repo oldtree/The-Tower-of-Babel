e66a3844222bbee77dee01822d822b765993fab4
tree | oldtree <treeui.old@gmail.com | 435594427@qq.com>
2014-01-22 22:26:16 +0800

    he he

--- models/projects.go
+++ models/projects.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 	"github.com/astaxie/beego/orm"
+	"sort"
 	"time"
 )
 
@@ -18,6 +19,9 @@ type Project struct {
 	User_email      string    `orm:"size(30)"`
 	Project_created time.Time `orm:"auto_now_add"`
 	Project_update  time.Time `orm:"auto_now"`
+	Star            int
+	User_count      int
+	Path            string `orm:"size(256)"`
 }
 
 func (p *Project) Insert() error {
@@ -41,4 +45,58 @@ func (p *Project) Update(fileds ...string) error {
 
 func Projects() orm.QuerySeter {
 	return orm.NewOrm().QueryTable("Projects").OrderBy("-Id")
+}
+
+type lessFunc func(p1, p2 *Project) bool
+
+type ProjectSorter struct {
+	projects []Project
+	less     []lessFunc
+}
+
+func (ms *ProjectSorter) Sort(Projects []Project) {
+	sort.Sort(ms)
+
+}
+
+func OrderedBy(projects []Project, less ...lessFunc) *ProjectSorter {
+	return &ProjectSorter{
+		projects: projects,
+		less:     less,
+	}
+}
+
+func (ms *ProjectSorter) Len() int {
+	return len(ms.projects)
+}
+
+func (ms *ProjectSorter) Swap(i, j int) {
+	ms.projects[i], ms.projects[j] = ms.projects[j], ms.projects[i]
+}
+
+func (ms *ProjectSorter) Less(i, j int) bool {
+	p, q := &ms.projects[i], &ms.projects[j]
+	var k int
+	for k = 0; k < len(ms.less)-1; k++ {
+		less := ms.less[k]
+		switch {
+		case less(p, q):
+			return true
+		case less(q, p):
+			return false
+		}
+	}
+	return ms.less[k](p, q)
+}
+
+// 文章列表按照日期和ID排序
+func SortProject(projects []Project) (projectsaftersort []Project) {
+	stars := func(p1, p2 *Project) bool {
+		return p1.Star > p2.Star
+	}
+	update := func(p1, p2 *Project) bool {
+		return p1.Project_update.After(p2.Project_update)
+	}
+	sort.Sort(OrderedBy(projects, update, stars))
+	return projects
 }
